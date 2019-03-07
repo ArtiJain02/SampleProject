c87419929535354e4f0b23cae9bb8bacf8fa2666
Jain,Arti <Arti.Jain@cerner.com>
2019-03-07 10:45:44 +0530

    committimg

--- first.js
+++ first.js
@@ -1,1 +1,4 @@
-console.log("hello world from sample folder file");
+console.log("now im changing the file and then i will commit it the sample brach then to the master branch");
+The motive of this is to check 
+how many lines have been
+modified while my process is being carried out.
